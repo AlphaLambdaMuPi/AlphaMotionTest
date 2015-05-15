75c42776fd682fb79d6d270e9120bc66463eecbf
LeoMao <LMao20001@gmail.com>
2015-05-15 14:16:07 +0800

    add gradle files

--- app/src/main/java/org/ntuee/leomao/motiontest/MainActivityFragment.java
+++ app/src/main/java/org/ntuee/leomao/motiontest/MainActivityFragment.java
@@ -15,11 +15,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -35,8 +38,8 @@ public class MainActivityFragment extends Fragment {
     private SensorManager smanager;
     private Sensor ameter;
     private Sensor gyroscope;
-    private Sensor gmeter;
-    private Sensor mmeter;
+//    private Sensor gmeter;
+//    private Sensor mmeter;
 
     private View rootview;
     private TextView cnttext = null;
@@ -46,14 +49,17 @@ public class MainActivityFragment extends Fragment {
     private Button startbtn;
     private Button stopbtn;
 
-    private double alpha = 0.2;
-    private float [] acc = new float[3];
-    private float [] omega = new float[3];
-    private float [] orientation = new float[3];
-    private float [] gravity = new float[3];
-    private float [] magnetic = new float[3];
-    private float [] rotationMatrix = new float[16];
-    private long lastacctime = 0;
+    private EditText hosttext;
+    private EditText porttext;
+
+//    private double alpha = 0.2;
+//    private float [] acc = new float[3];
+//    private float [] omega = new float[3];
+//    private float [] orientation = new float[3];
+//    private float [] gravity = new float[3];
+//    private float [] magnetic = new float[3];
+//    private float [] rotationMatrix = new float[16];
+//    private long lastacctime = 0;
     private int datacnt = 0;
 
     private OutputStreamWriter writer = null;
@@ -65,13 +71,18 @@ public class MainActivityFragment extends Fragment {
     private boolean connected = false;
     private boolean started = false;
 
-    private final int DELTA_T = 5;
+    private String datahost = "";
+    private int port = 0;
+
+//    private final int DELTA_T = 5;
 
     private SensorEventListener ahandler = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            lastacctime = event.timestamp;
-            System.arraycopy(event.values, 0, acc, 0, 3);
+            if (started) {
+                socketHandler.post(new DataSender("accel", event.values,
+                        event.timestamp));
+            }
         }
 
         @Override
@@ -83,8 +94,10 @@ public class MainActivityFragment extends Fragment {
     private SensorEventListener rhandler = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
-            lastacctime = event.timestamp;
-            System.arraycopy(event.values, 0, omega, 0, 3);
+            if (started) {
+                socketHandler.post(new DataSender("gyroscope", event.values,
+                        event.timestamp));
+            }
         }
 
         @Override
@@ -93,42 +106,49 @@ public class MainActivityFragment extends Fragment {
         }
     };
 
-    private SensorEventListener ghandler = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            lastacctime = event.timestamp;
-            System.arraycopy(event.values, 0, gravity, 0, 3);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-    private SensorEventListener mhandler = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            lastacctime = event.timestamp;
-            System.arraycopy(event.values, 0, magnetic, 0, 3);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
-
-    private void getOrientation() {
-        SensorManager.getRotationMatrix(rotationMatrix, null, gravity, magnetic);
-        SensorManager.getOrientation(rotationMatrix, orientation);
-    }
+//    private SensorEventListener ghandler = new SensorEventListener() {
+//        @Override
+//        public void onSensorChanged(SensorEvent event) {
+//            if (started) {
+//
+//            }
+//            lastacctime = event.timestamp;
+//            System.arraycopy(event.values, 0, gravity, 0, 3);
+//            getOrientation();
+//        }
+//
+//        @Override
+//        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//        }
+//    };
+//
+//    private SensorEventListener mhandler = new SensorEventListener() {
+//        @Override
+//        public void onSensorChanged(SensorEvent event) {
+//            lastacctime = event.timestamp;
+//            System.arraycopy(event.values, 0, magnetic, 0, 3);
+//            getOrientation();
+//        }
+//
+//        @Override
+//        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//        }
+//    };
+//
+//    private void getOrientation() {
+//        SensorManager.getRotationMatrix(rotationMatrix, null, gravity, magnetic);
+//        SensorManager.getOrientation(rotationMatrix, orientation);
+//    }
 
     private Runnable starter = new Runnable() {
         @Override
         public void run() {
+            if (datahost.equals("") || port == 0)
+                return;
             try {
-                socket = new Socket("140.112.18.210", 12345);
+                socket = new Socket(datahost, port);
                 writer = new OutputStreamWriter(socket.getOutputStream());
                 reader = new InputStreamReader(socket.getInputStream());
                 connected = true;
@@ -137,6 +157,7 @@ public class MainActivityFragment extends Fragment {
             }
             catch (Exception e) {
                 connected = false;
+                started = false;
                 Log.d("DEBUG", "connection failed");
                 Log.d("DEBUG", e.getMessage());
                 return;
@@ -148,55 +169,41 @@ public class MainActivityFragment extends Fragment {
                 String s = data.toString() + "\n";
                 writer.write(s);
             }
-            catch (Exception e) {
-                Log.d("DEBUG", "socket write error");
-            }
-            socketHandler.postDelayed(updater, DELTA_T);
-        }
-    };
-
-    private Runnable updater = new Runnable() {
-        @Override
-        public void run() {
-            if (socket == null || socket.isClosed())
-                return;
-
-            if (datacnt >= 5000) {
-                socketHandler.post(stoper);
-                return;
-            }
-
-            getOrientation();
-            JSONObject data = new JSONObject();
-            try {
-                data.put("time", lastacctime);
-                String accstr = String.format("[%.4f,%.4f,%.4f]",
-                        acc[0], acc[1], acc[2]);
-                String omegastr = String.format("[%.4f,%.4f,%.4f]",
-                        omega[0], omega[1], omega[2]);
-                String oristr = String.format("[%.4f,%.4f,%.4f]",
-                        orientation[0], orientation[1], orientation[2]);
-                data.put("acc", accstr);
-                data.put("omega", omegastr);
-                data.put("orientation", oristr);
-                String s = data.toString() + "\n";
-                writer.write(s);
-            }
-            catch (Exception e) {
+            catch (JSONException e) {
                 Log.d("DEBUG", "JSON object error");
             }
-            datacnt += 1;
-            UIHandler.post(updateUI);
-            if (started)
-                socketHandler.postDelayed(updater, DELTA_T);
+            catch (IOException e) {
+                Log.d("DEBUG", "socket write error");
+                if (socket.isInputShutdown())
+                    socketHandler.post(stoper);
+            }
         }
     };
+
+//    private Runnable updater = new Runnable() {
+//        @Override
+//        public void run() {
+//            if (socket == null || socket.isClosed())
+//                return;
+//
+//            if (datacnt >= 5000) {
+//                socketHandler.post(stoper);
+//                return;
+//            }
+//
+//            getOrientation();
+//            datacnt += 1;
+//            UIHandler.post(updateUI);
+//            if (started)
+//                socketHandler.postDelayed(updater, DELTA_T);
+//        }
+//    };
 
     private Runnable stoper = new Runnable() {
         @Override
         public void run() {
             started = false;
-            socketHandler.removeCallbacks(updater);
+//            socketHandler.removeCallbacks(updater);
             JSONObject data = new JSONObject();
             try {
                 data.put("stop", true);
@@ -204,13 +211,56 @@ public class MainActivityFragment extends Fragment {
                 writer.write(s);
                 writer.flush();
                 socket.close();
-                socket = null;
             }
-            catch (Exception e) {
-                Log.d("DEBUG", "socket write error");
+            catch (JSONException e) {
+                Log.d("DEBUG", "JSON object error");
             }
+            catch (IOException e) {
+                Log.d("DEBUG", "socket was already closed");
+            }
+            socket = null;
         }
     };
+
+    private class DataSender implements Runnable {
+        private String type;
+        private float [] data = new float[3];
+        private long time;
+        public DataSender(String type, float[] data, long time) {
+            this.type = type;
+            this.time = time;
+            System.arraycopy(data, 0, this.data, 0, 3);
+        }
+
+        @Override
+        public void run() {
+            if (socket == null || socket.isClosed() || !started)
+                return;
+
+            JSONObject data = new JSONObject();
+            try {
+                data.put("time", this.time);
+                String datastr = String.format("[%.4f,%.4f,%.4f]",
+                        this.data[0], this.data[1], this.data[2]);
+                data.put("data", datastr);
+                data.put("time", this.time);
+                data.put("type", this.type);
+                String s = data.toString() + "\n";
+                writer.write(s);
+                datacnt += 1;
+                UIHandler.post(updateUI);
+            }
+            catch (JSONException e) {
+                Log.d("DEBUG", "JSON object error");
+            }
+            catch (IOException e) {
+                Log.d("DEBUG", "socket write error");
+                if (socket.isInputShutdown())
+                    socketHandler.post(stoper);
+            }
+        }
+
+    }
 
     private Runnable updateUI = new Runnable() {
         @Override
@@ -231,11 +281,15 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.fragment_main, container, false);
         cnttext = (TextView) rootview.findViewById(R.id.acctext);
+        hosttext = (EditText) rootview.findViewById(R.id.datahost);
+        porttext = (EditText) rootview.findViewById(R.id.port);
         startbtn = (Button) rootview.findViewById(R.id.start);
         stopbtn = (Button) rootview.findViewById(R.id.stop);
         startbtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+                datahost = hosttext.getText().toString();
+                port = Integer.parseInt(porttext.getText().toString());
                 socketHandler.post(starter);
                 // if (connected) {
                 //     started = true;
@@ -269,8 +323,8 @@ public class MainActivityFragment extends Fragment {
         smanager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
         ameter = smanager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         gyroscope = smanager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        gmeter = smanager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        mmeter = smanager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+//        gmeter = smanager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+//        mmeter = smanager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         registerListener();
     }
 
@@ -295,15 +349,15 @@ public class MainActivityFragment extends Fragment {
     private void registerListener() {
         smanager.registerListener(ahandler, ameter, SensorManager.SENSOR_DELAY_FASTEST);
         smanager.registerListener(rhandler, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
-        smanager.registerListener(ghandler, gmeter, SensorManager.SENSOR_DELAY_FASTEST);
-        smanager.registerListener(mhandler, mmeter, SensorManager.SENSOR_DELAY_FASTEST);
+//        smanager.registerListener(ghandler, gmeter, SensorManager.SENSOR_DELAY_FASTEST);
+//        smanager.registerListener(mhandler, mmeter, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     private void unRegisterListener() {
         smanager.unregisterListener(ahandler);
         smanager.unregisterListener(rhandler);
-        smanager.unregisterListener(ghandler);
-        smanager.unregisterListener(mhandler);
+//        smanager.unregisterListener(ghandler);
+//        smanager.unregisterListener(mhandler);
     }
 
 
